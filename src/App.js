64065a3fb5fddda239e2a37fbc0e34887a182e5d
Heikki Ahokas <heikki.ahokas@esedulainen.fi>
2022-03-12 03:08:25 +0200

    Muutokset.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React, {useState} from "react";
+import React, {useState, useRef, useEffect} from "react";
 import TodoList from "./TodoList";
+import { v4 as uuidv4 } from 'uuid'
 
 function App() {
-  useState([])
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  useEffect(() => {
+
+  }, [todos])
+
+  function handleAddtodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setTodos(x => {
+      return [...x, {id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+  }
   return (
     <>
-    <TodoList />
-    <input type="text" />
-    <button>Add Todo</button>
+    <TodoList todos ={todos} />
+    <input ref={todoNameRef} type="text" />
+    <button onClick={handleAddtodo}>Add Todo</button>
     <button>Clear completed Todos</button>
     <div>0 left to do</div>
     </>
