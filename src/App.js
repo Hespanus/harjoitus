b546e0d16e0432b8f02562851fd09279a80bf990
Heikki Ahokas <heikki.ahokas@esedulainen.fi>
2022-03-14 01:17:54 +0200

    uudestaan

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import React, {useState, useRef, useEffect} from "react";
 import TodoList from "./TodoList";
 import { v4 as uuidv4 } from 'uuid'
+import { Routes, Route } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+import Article from "./Article";
+
+const STORAGE_KEY = 'todoApp.todos'
 
 function App() {
+
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
 
   useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+  
 
+  useEffect(() => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
   }, [todos])
 
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+    
   function handleAddtodo(e) {
     const name = todoNameRef.current.value
     if (name === '') return
@@ -18,13 +37,30 @@ function App() {
     })
     todoNameRef.current.value = null
   }
+
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
   return (
     <>
-    <TodoList todos ={todos} />
+    
+      <Routes>
+       
+          
+          <Route path="/article" element={<Article />} />         
+        
+      </Routes>
+    
+    <Link to="/article">Artikkeli</Link>
+
+    <Outlet />
+    
+    <TodoList todos ={todos} toggleTodo={toggleTodo} />
     <input ref={todoNameRef} type="text" />
     <button onClick={handleAddtodo}>Add Todo</button>
-    <button>Clear completed Todos</button>
-    <div>0 left to do</div>
+    <button onClick={handleClearTodos}>Clear completed Todos</button>
+    <div>{todos.filter(todo => !todo.complete).length}left to do</div>
     </>
     
   )
