440d0f51f8d711fef2123828b67651a32e1e5b75
Heikki Ahokas <heikki.ahokas@esedulainen.fi>
2022-10-05 19:13:45 +0300

    t

--- frontend/src/Article.js
+++ frontend/src/Article.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function article() {
   return (
     <div>
-        Jotain tekstiä vähän lisää ja vieläkin ynnä lisää, testi1, testi2, testi3, testi4, testi5, testi6, testi7
+        Jotain tekstiä vähän lisää ja vieläkin ynnä lisää, testi1, testi2, testi3, testi4, testi5, testi6, testi7, testi8
       
     </div>
   )
