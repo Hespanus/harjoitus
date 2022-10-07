5f0d9563574296eace1e2487f16e0ff1623a3023
Heikki Ahokas <heikki.ahokas@esedulainen.fi>
2022-10-08 00:47:55 +0300

    ssf

--- backend/connection.js
+++ backend/connection.js
@@ -3,7 +3,7 @@ const Todo = require("./Todo.model.js")
 
 
 
-const connection = "mongodb://${process.env.DB_NAME}:${process.env.DB_PASS}@mongo:27017/todotest";
+const connection = `mongodb://${process.env.DB_NAME}:${process.env.DB_PASS}@mongo:27017/todotest`;
 
 const connectDb = () => {
     return mongoose.connect(connection);
