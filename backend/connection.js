409cdfd7818e9f156302c8071dadfa3bf77ef48a
Heikki Ahokas <heikki.ahokas@esedulainen.fi>
2022-10-07 22:12:35 +0300

    conn dotenv

--- backend/connection.js
+++ backend/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Todo = require("./Todo.model.js")
 
+require('dotenv').config();
+
 const connection = "mongodb://${process.env.DB_NAME}:${process.env.DB_PASS}@mongo:27017/todotest";
 
 const connectDb = () => {
